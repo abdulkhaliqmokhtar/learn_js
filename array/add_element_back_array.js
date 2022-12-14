818745ed5a38797bfed721b688f169ccc9a88b91
abdulkhaliqmokhtar <abdulkhaliqmokhtar@gmail.com>
2022-12-14 20:47:07 +0800

    Fix comment mistake

--- array/add_element_back_array.js
+++ array/add_element_back_array.js
@@ -3,5 +3,5 @@ let testArray = [1, 2, 3, 4, 5];
 // add integer 6 to the back of testArray using .push
 let newArray = testArray.push(6);
 
-// print newArray containing integer 6
+// print testArray containing integer 6
 console.log(testArray);
--- array/remove_element_back_array.js
+++ array/remove_element_back_array.js
@@ -3,5 +3,5 @@ let testArray = [1, 2, 3, 4, 5];
 // remove integer 5 from the back of testArray using .pop
 let newArray = testArray.pop(5);
 
-// print newArray which doesn't contain integer 5
+// print testArray which doesn't contain integer 5
 console.log(testArray);
