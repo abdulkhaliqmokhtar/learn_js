d0eb128743d8c069b38f778e1206c9ac8431edb6
abdulkhaliqmokhtar <abdulkhaliqmokhtar@gmail.com>
2022-12-14 20:38:56 +0800

    Basic of array manipulation

---
+++ array/add_element_back_array.js
@@ -0,0 +1,7 @@
+let testArray = [1, 2, 3, 4, 5];
+
+// add integer 6 to the back of testArray using .push
+let newArray = testArray.push(6);
+
+// print newArray containing integer 6
+console.log(testArray);
