8f983cdbe361653b3fac03bcd3cc4ba6b27e3083
abdulkhaliqmokhtar <abdulkhaliqmokhtar@gmail.com>
2022-12-14 20:45:46 +0800

    Add element in the front of testArray

---
+++ array/add_element_front_array.js
@@ -0,0 +1,7 @@
+let testArray = [1, 2, 3, 4, 5];
+
+// add integer 0 in the front of testArray using .unshift
+let newArray = testArray.unshift(0);
+
+// print testArray which contain the new integer 0
+console.log(testArray);
