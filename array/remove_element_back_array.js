fc250df14087232eef61f848adf7c39a9f2dd724
abdulkhaliqmokhtar <abdulkhaliqmokhtar@gmail.com>
2022-12-14 20:42:00 +0800

    Remove element at the back of testArray

---
+++ array/remove_element_back_array.js
@@ -0,0 +1,7 @@
+let testArray = [1, 2, 3, 4, 5];
+
+// remove integer 5 from the back of testArray using .pop
+let newArray = testArray.pop(5);
+
+// print newArray which doesn't contain integer 5
+console.log(testArray);
