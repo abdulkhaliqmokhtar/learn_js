ed5a824c624bbf3489c1db67b729a3512eaba1f9
abdulkhaliqmokhtar <abdulkhaliqmokhtar@gmail.com>
2022-12-18 21:48:58 +0800

    Function with multiple input parameters

---
+++ function/multiple_input.js
@@ -0,0 +1,11 @@
+// create function that takes 2 parameters: base and height
+function areaOfTriangle(base, height) {
+  return (base * height) / 2;
+}
+
+// call the areaOfTriangle function to calculate the area of a triangle with base of 7 and height of 4
+// store the value in the are variable
+let area = areaOfTriangle(7, 4);
+
+// print value stored in area
+console.log("The area of triangle is " + area);
