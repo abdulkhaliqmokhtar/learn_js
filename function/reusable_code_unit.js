6bfe6b5e24d4fb1205fcff135b4fc8e587fb042c
abdulkhaliqmokhtar <abdulkhaliqmokhtar@gmail.com>
2022-12-18 21:09:55 +0800

    Create a basic function

---
+++ function/reusable_code_unit.js
@@ -0,0 +1,12 @@
+// create a simple function that output a simple greet and a question
+function askForLunch() {
+  console.log("Hello, my friend!");
+  console.log("Have you got your lunch yet?");
+}
+
+// call the askForLunch function 5 times
+askForLunch();
+askForLunch();
+askForLunch();
+askForLunch();
+askForLunch();
