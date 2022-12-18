24128b7c15c08f9345f29bccad3e854a9ab8dc03
abdulkhaliqmokhtar <abdulkhaliqmokhtar@gmail.com>
2022-12-18 21:40:26 +0800

    Basic arithmetic function

---
+++ function/simple_calculator.js
@@ -0,0 +1,10 @@
+// create function that always divide the inputNumber by 5, and return the answer
+function divideByFive(inputNumber) {
+  return inputNumber / 5;
+}
+
+// call the function with input value of 100 and store the calculation result in a answer variable
+let answer = divideByFive(100);
+
+// print value stored in answer variable
+console.log(answer);
